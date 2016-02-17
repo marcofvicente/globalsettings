cf18908bb8231b90fb8d16924e7dcc9642363a4a
Marco Vicente <marco@MacBook-Pro.rede.local>
2016-02-17 23:54:43 +0000

    Initial commit

---
+++ config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Install brew    
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install wget
+brew install nmap
+brew install watch
+brew install tmux
